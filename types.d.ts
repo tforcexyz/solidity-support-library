06a5257f1c49bb65e03ca7d9aab369f192035443
Lukaz Precision <30133485+lukazvprecisn@users.noreply.github.com>
2023-06-12 11:40:00 +0700

    hash and merkle tree support

--- types.d.ts
+++
@@ -1,4 +0,0 @@
-export { Factory } from './typechain-types/contracts/Factory';
-export { Factory__factory } from './typechain-types/factories/contracts/Factory__factory';
-export { MasterCopy } from './typechain-types/contracts/MasterCopy';
-export { MasterCopy__factory } from './typechain-types/factories/contracts/MasterCopy__factory';
